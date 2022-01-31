c5a2541820b58385798106ad15ff0ef52eaf8917
akQuadSat <ak@quadsat.com>
2022-01-31 22:36:48 +0100

    Fixes syntax job

--- exchangemarket/exchangemarket/settings.py
+++ exchangemarket/exchangemarket/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_template_check',
     'exchange_market_app',
     'django.contrib.admin',
     'django.contrib.auth',
